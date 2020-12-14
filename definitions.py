1306248e06b1d2e78c5278e5603dec057d6b1eac
f80dev <rv@f80.fr>
2020-12-14 14:14:43 +0100

    Version opérationnelle pour test

--- definitions.py
+++ definitions.py
@@ -6,7 +6,7 @@ IMAP_SERVER="imap.ionos.fr"
 APPNAME="CoinMaKer"
 
 #DOMAIN_APPLI="http://localhost:4200"
-DOMAIN_APPLI="https://coinmaker.f80.fr"
+DOMAIN_APPLI="https://tf.f80.fr"
 ERC20_BYTECODE_PATH="./static/deploy-erc20.json"
 NFT_BYTECODE_PATH="./static/deploy-nft.json"
 
@@ -18,7 +18,7 @@ XGLD_FOR_NEWACCOUNT="500000000000000000"
 
 #Description de la monnaie par defaut
 MAIN_UNITY="TFC"
-MAIN_URL="https://coinmaker.f80.fr/assets/cmk.html"
+MAIN_URL="https://tf.f80.fr/assets/cmk.html"
 TOTAL_DEFAULT_UNITY=1000000
 CREDIT_FOR_NEWACCOUNT=75
 DEFAULT_UNITY_CONTRACT="erd1qqqqqqqqqqqqqpgqt4zcz5q0xytmefh5jsdfntf6zxfyv0t3d8sslcts96"
@@ -43,5 +43,5 @@ MAIN_DEVISE="xEGld"
 #NFT pour le testnet officiel
 #NFT_CONTRACT="erd1qqqqqqqqqqqqqpgqfx6ndl75schd9y4ulkhj2vq26j93kgued8ssd9pv42"
 
-NFT_CONTRACT="erd1qqqqqqqqqqqqqpgqr3yt02u59g2hwptpeawd5z8vt8qete0sd8ss6x8cnu"
+NFT_CONTRACT="erd1qqqqqqqqqqqqqpgqs0a05q6p2x8g2th3yrssq8dqmyp8m94ud8ss2ecs5c"
 NFT_ADMIN="admin"
