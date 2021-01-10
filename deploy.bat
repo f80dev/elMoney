7bc6f5b209cdcb5aae8284652950a7e9e4425197
f80dev <rv@f80.fr>
2021-01-10 21:05:23 +0100

    version avec ESDT ok mais probleme sur NFT (en particulier URI)

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,6 @@ start npm run prod
 
 cd ..
 docker build -t f80hub/elmoney .
-start docker push f80hub/elmoney:latest
+docker push f80hub/elmoney:latest
 
 echo "Déployment terminer, poussez sur github"
