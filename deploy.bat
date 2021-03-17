3c4579f4ec86f96eba3b26978fe496058a9116fb
f80dev <rv@f80.fr>
2021-03-17 15:30:25 +0100

    Modification du fichier excel

--- deploy.bat
+++ deploy.bat
@@ -2,8 +2,10 @@ cd elMoneyAppli
 start npm run dev
 
 cd ..
-docker build -t f80hub/elmoney .
-docker push f80hub/elmoney:latest
+docker build -t f80hub/elmoney-dev .
+docker push f80hub/elmoney-dev:latest
 
 echo "Déployment terminer, poussez sur github"
+
+echo "Déployer l'image sur le serveur via docker rm -f elmoney-dev && docker pull f80hub/elmoney-dev && docker run --restart=always -v /root/certs:/certs -p 7777:7777 --name elmoney-dev -d f80hub/elmoney:latest python3 app.py 7777 devnet coinmaker ssl"
 start putty -load MainServer -l root
