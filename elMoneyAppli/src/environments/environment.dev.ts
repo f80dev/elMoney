83ff6649ab05702f80a59df62286d59782ae00e6
f80dev <herve.hoareau@f80.fr>
2021-09-03 13:54:08 +0200

    connection avec la page du wallet elrond

--- elMoneyAppli/src/environments/environment.dev.ts
+++ elMoneyAppli/src/environments/environment.dev.ts
@@ -1,11 +1,12 @@
-
+// @ts-ignore
+import { version } from "../../package.json";
 
 export const environment = {
   production: false,
   domain_server: "https://server.f80.fr:7777",
   config_file: "config",
   domain_appli: "https://tf.f80.fr",
-  appVersion: "0.0.1",
+  appVersion: version,
   default_contract: "erd1qqqqqqqqqqqqqpgqeyayz09s2a4gnvcghdh9ma3he3j7cda0d8ss2apk2a",
   transac_cost: 0.001
 };
