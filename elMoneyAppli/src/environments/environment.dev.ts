018c0277e4424aaceb4494260dfe7df551444ab1
F80 <43803343+f80dev@users.noreply.github.com>
2022-01-22 19:43:22 +0100

    Correction sur le transfert

--- elMoneyAppli/src/environments/environment.dev.ts
+++ elMoneyAppli/src/environments/environment.dev.ts
@@ -7,7 +7,7 @@ export const environment = {
   idcardserver:"http://server.f80.fr:2222/api/upload/",
   config_file: "config",
   domain_appli: "https://tf.f80.fr",
-  appVersion: "1.0.1",
+  appVersion: "1.0.2",
   default_contract: "erd1qqqqqqqqqqqqqpgqeyayz09s2a4gnvcghdh9ma3he3j7cda0d8ss2apk2a",
   transac_cost: 0.001
 };
